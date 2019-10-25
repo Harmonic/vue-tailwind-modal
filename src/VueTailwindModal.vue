d709c55f40f49ad318696c66e72eb68bb93a81a2
Craig Harman <craig@harmonicnewmedia.com>
2019-10-25 12:44:51 +0800

    Add smoke to modal style

--- src/VueTailwindModal.vue
+++ src/VueTailwindModal.vue
@@ -2,7 +2,7 @@
   <Transition name="fade">
     <div
       v-if="showing"
-      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-smoke-dark"
+      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-smoke-800"
       @click.self="close"
     >
       <div class="relative max-h-screen w-full max-w-2xl bg-white shadow-lg rounded-lg p-8 flex">
@@ -54,4 +54,12 @@ export default {
 .fade-leave-to {
   opacity: 0;
 }
+
+.bg-smoke-900 {background-color: rgba(0, 0, 0, 0.9)}
+.bg-smoke-800 {background-color: rgba(0, 0, 0, 0.75)}
+.bg-smoke-600 {background-color: rgba(0, 0, 0, 0.6)}
+.bg-smoke {background-color: rgba(0, 0, 0, 0.5)}
+.bg-smoke-400 {background-color: rgba(0, 0, 0, 0.4)}
+.bg-smoke-200 {background-color: rgba(0, 0, 0, 0.25)}
+.bg-smoke-100 {background-color: rgba(0, 0, 0, 0.1)}
 </style>
